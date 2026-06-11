a58053915624d8181b107ca0bdaaf1b462e86baa
Aryan Yadav <caryan830@gmail.com>
2026-06-11 21:31:32 +0530

    implement socket.io

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import {createServer} from "http"
+import { Server } from "socket.io"
+import { YSocketIO } from "y-socket.io/dist/server"
+const app  = express()
+const httpServer = createServer(app)
+
+
+const io = new Server(httpServer,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"]
+    }
+})
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"HEY I am aryan",
+        success:true
+    
+    })
+})
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({
+        message:"ok",
+        success:true
+    })
+})
+
+httpServer.listen(3000,()=>{
+    console.log("server is running in the port 3000")
+})
+
+
+
+
+
+
+
+
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
 import { Editor } from "@monaco-editor/react"
-
-
+import { MonacoBinding} from "y-monaco" 
+import { useRef,useMemo } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 const App = () => {
+
+  const editorRef= useRef(null)
+
+  const ydoc = useMemo(()=>new Y.Doc(),[])                //use for  doc file thius the conver in ydoc
+  const yText = useMemo(()=>ydoc.getText("monaco"),[ydoc])
+
+  const handeMount = (editor) =>{
+    editorRef.current = editor
+
+    const provder = new SocketIOProvider("http://localhost:3000/" , "monaco" ,ydoc,{
+      autoConnect:true,
+    })
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set ([editorRef.current]),
+      provder.awareness
+
+    )
+
+  }
+
   return (
     <main
     className="h-screen w-full bg-gray-950 flex gap-4 p-4"
@@ -18,6 +42,7 @@ const App = () => {
         defaultLanguage="javascript"
         defaultValue="// soem comment"
         theme="vs-dark"
+        onMount={handeMount}
         />
 
 
