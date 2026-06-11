9702d7ebbfb31c71e427296538fc7c5f283c0cbe
Aryan Yadav <caryan830@gmail.com>
2026-06-11 17:40:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Editor } from "@monaco-editor/react"
+
+
+
+const App = () => {
+  return (
+    <main
+    className="h-screen w-full bg-gray-950 flex gap-4 p-4"
+    >
+
+      <aside
+      className="h-full w-1/4 bg-amber-50 rounded-lg ">
+      </aside>
+      <section
+      className="w-3/4 bg-gray-800 rounded-lg">
+        <Editor 
+        height="100%"
+        defaultLanguage="javascript"
+        defaultValue="// soem comment"
+        theme="vs-dark"
+        />
+
+
+      </section>
+
+    </main>
+  )
+}
+
+export default App
